512d4f49b7df7ef53f509c6e7346da4e5b7b62a1
CY Chen <cychen0429@gmail.com>
2016-05-16 10:40:21 +0800

    modify to search pixnet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,15 +17,23 @@ class PagesController < ApplicationController
       require 'json'
 
       # Use Text Search
-        google_place_url = "https://maps.googleapis.com/maps/api/place/"
-        search_method = "textsearch/json?language=zh-TW&"
-        query_string = "query=景點+in+#{params[:search_string]}"
-        api_key = "&key=#{ENV["google_api_key"]}"
-        url = google_place_url + search_method + query_string + api_key
+        # google_place_url = "https://maps.googleapis.com/maps/api/place/"
+        # search_method = "textsearch/json?language=zh-TW&"
+        # query_string = "query=景點+in+#{params[:search_string]}"
+        # api_key = "&key=#{ENV["google_api_key"]}"
+        # url = google_place_url + search_method + query_string + api_key
+        # encoded_url = URI.encode(url)
+        # uri = URI.parse(encoded_url)
+        # @response = JSON.parse(Net::HTTP.get(uri))["results"]
+
+      # Search pixnet
+      # https://emma.pixnet.cc/blog/articles/search?key=沖繩&per_page=10&format=json      
+        pixnet_place_url = "https://emma.pixnet.cc/blog/articles/search?&per_page=10&format=json&"
+        query_string = "key=#{params[:search_string]}"
+        url = pixnet_place_url + query_string 
         encoded_url = URI.encode(url)
         uri = URI.parse(encoded_url)
-        @response = JSON.parse(Net::HTTP.get(uri))["results"]
-
+        @articles = JSON.parse(Net::HTTP.get(uri))["articles"]
 
     end    
     
@@ -36,33 +44,55 @@ class PagesController < ApplicationController
       require 'json'
 
       # Use Text Search
-        google_place_url = "https://maps.googleapis.com/maps/api/place/"
-        search_method = "textsearch/json?language=zh-TW&"
-        query_string = "query=景點+in+#{params[:search_string]}"
-        api_key = "&key=#{ENV["google_api_key"]}"
-        url = google_place_url + search_method + query_string + api_key
+        # google_place_url = "https://maps.googleapis.com/maps/api/place/"
+        # search_method = "textsearch/json?language=zh-TW&"
+        # query_string = "query=景點+in+#{params[:search_string]}"
+        # api_key = "&key=#{ENV["google_api_key"]}"
+        # url = google_place_url + search_method + query_string + api_key
+        # encoded_url = URI.encode(url)
+        # uri = URI.parse(encoded_url)
+        # @response = JSON.parse(Net::HTTP.get(uri))["results"]    
+        # @response_json = Array.new
+
+        # @response.each_with_index do |r, i|
+        #   if r["photos"]
+        #     tmp = Hash.new
+        #     tmp["name"] = r["name"]
+        #     tmp["address"] = r["address"]
+        #     tmp["lat"] = r["geometry"]["location"]["lat"]
+        #     tmp["lng"] = r["geometry"]["location"]["lng"]
+        #     tmp["photo_url"] =  "https://maps.googleapis.com/maps/api/place/photo?maxwidth=#{r["photos"][0]["width"]}&photoreference=" + r["photos"][0]["photo_reference"] + "&key=#{ENV["google_api_key"]}"
+        #     tmp["photo_width"] = r["photos"][0]["width"]
+        #     tmp["photo_height"] = r["photos"][0]["height"]       
+        #     tmp["place_id"] = r["place_id"]
+        #     tmp["rating"] = r["rating"]
+        #     @response_json << tmp
+        #   end
+        # end
+
+        # render json: @response_json
+
+      # Search pixnet
+        pixnet_place_url = "https://emma.pixnet.cc/blog/articles/search?&per_page=10&format=json&"
+        query_string = "key=#{params[:search_string]}"
+        url = pixnet_place_url + query_string 
         encoded_url = URI.encode(url)
         uri = URI.parse(encoded_url)
-        @response = JSON.parse(Net::HTTP.get(uri))["results"]    
-        @response_json = Array.new
+        @articles = JSON.parse(Net::HTTP.get(uri))["articles"]
+        @article_json = Array.new
 
-        @response.each_with_index do |r, i|
-          if r["photos"]
-            tmp = Hash.new
-            tmp["name"] = r["name"]
-            tmp["address"] = r["address"]
-            tmp["lat"] = r["geometry"]["location"]["lat"]
-            tmp["lng"] = r["geometry"]["location"]["lng"]
-            tmp["photo_url"] =  "https://maps.googleapis.com/maps/api/place/photo?maxwidth=#{r["photos"][0]["width"]}&photoreference=" + r["photos"][0]["photo_reference"] + "&key=#{ENV["google_api_key"]}"
-            tmp["photo_width"] = r["photos"][0]["width"]
-            tmp["photo_height"] = r["photos"][0]["height"]       
-            tmp["place_id"] = r["place_id"]
-            tmp["rating"] = r["rating"]
-            @response_json << tmp
-          end
+        @articles.each_with_index do |a, i|
+          tmp = Hash.new
+          tmp["title"] = a["title"]
+          tmp["photo_url"] =  a["link"]
+          tmp["photo_width"] = "90"
+          tmp["photo_height"] = "90" 
+          tmp["category"] = a["category"]
+          tmp["site_category"] = a["site_category"]
+          @article_json << tmp
         end
 
-        render json: @response_json
+        render json: @article_json
   end
 
 

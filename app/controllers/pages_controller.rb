7ebc0115fc2e0b759ef097b050ff6b65fc4e0808
CYChen <cychen0429@gmail.com>
2016-06-14 18:55:07 +0800

    fix near_by

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -37,11 +37,33 @@ class PagesController < ApplicationController
   end
 
   def near_by
-    wow_search_pid_url = ENV["api_search_by_pid_url"] + "?pid=#{params[:pid]}&opt=#{params[:opt]}"
-    encoded_url = URI.encode(wow_search_pid_url)
-    uri = URI.parse(encoded_url)
-    @places = JSON.parse(Net::HTTP.get(uri))      
-    get_api_count
+    if params[:token]
+      wow_get_next_page_url = ENV["api_get_next_page_url"] + "?token=#{params[:token]}"
+      encoded_url = URI.encode(wow_get_next_page_url)
+      uri = URI.parse(encoded_url)
+      @places = JSON.parse(Net::HTTP.get(uri))      
+      @auto_complete_str = params[:auto_complete_str]
+      @auto_complete_lat = params[:auto_complete_lat]
+      @auto_complete_lng = params[:auto_complete_lng]  
+      @pid = params[:pid]
+      @str = params[:str]
+      @opt = params[:opt]
+      @redirect = params[:redirect]       
+      get_api_count   
+    else
+      wow_search_pid_url = ENV["api_search_by_pid_url"] + "?pid=#{params[:pid]}&opt=#{params[:opt]}"
+      encoded_url = URI.encode(wow_search_pid_url)
+      uri = URI.parse(encoded_url)
+      @places = JSON.parse(Net::HTTP.get(uri))      
+      @auto_complete_str = params[:str]
+      @auto_complete_lat = @places["auto_complete_lat"] 
+      @auto_complete_lng = @places["auto_complete_lng"]       
+      @pid = params[:pid]
+      @str = params[:ori_str]
+      @opt = params[:ori_opt]
+      @redirect = params[:redirect]    
+      get_api_count
+    end
   end
 
   private

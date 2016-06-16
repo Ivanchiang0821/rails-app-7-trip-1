b3b7cb14f18adfbf9ef85769009fb551a2a77d6e
CYChen <cychen0429@gmail.com>
2016-06-16 17:20:15 +0800

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,29 +3,34 @@ class PagesController < ApplicationController
   require 'json'
 
   def index
-    
+  end
+
+  def keyword
     if params[:token]
       wow_get_next_page_url = ENV["api_get_next_page_keyword_url"] + "?token=#{params[:token]}"
       encoded_url = URI.encode(wow_get_next_page_url)
       uri = URI.parse(encoded_url)
-      @places = JSON.parse(Net::HTTP.get(uri))      
-      @auto_complete_str = params[:auto_complete_str]
-      @auto_complete_lat = params[:auto_complete_lat]
-      @auto_complete_lng = params[:auto_complete_lng]  
-      get_api_count      
-    elsif params[:str]
+      @places = JSON.parse(Net::HTTP.get(uri))            
+    else
       wow_search_keyword_url = ENV["api_search_by_keyword_url"] + "?str=#{params[:str]}&opt=#{params[:opt]}"
       encoded_url = URI.encode(wow_search_keyword_url)
       uri = URI.parse(encoded_url)
       @places = JSON.parse(Net::HTTP.get(uri))      
-      @auto_complete_str = @places["auto_complete_str"]
-      @auto_complete_lat = @places["auto_complete_lat"] 
-      @auto_complete_lng = @places["auto_complete_lng"] 
-      get_api_count
-    else  
-      get_api_count
-    end  
-    
+    end      
+  end
+
+  def nearby
+    if params[:token]
+      wow_get_next_page_url = ENV["api_get_next_page_pid_url"] + "?token=#{params[:token]}"
+      encoded_url = URI.encode(wow_get_next_page_url)
+      uri = URI.parse(encoded_url)
+      @places = JSON.parse(Net::HTTP.get(uri)) 
+    else
+      wow_search_pid_url = ENV["api_search_by_pid_url"] + "?pid=#{params[:pid]}&opt=#{params[:opt]}"
+      encoded_url = URI.encode(wow_search_pid_url)
+      uri = URI.parse(encoded_url)
+      @places = JSON.parse(Net::HTTP.get(uri))      
+    end
   end
 
   def detail
@@ -33,45 +38,20 @@ class PagesController < ApplicationController
     encoded_url = URI.encode(wow_detail_url)
     uri = URI.parse(encoded_url)
     @place = JSON.parse(Net::HTTP.get(uri))      
-    get_api_count
   end
 
-  def near_by
-    if params[:token]
-      wow_get_next_page_url = ENV["api_get_next_page_pid_url"] + "?token=#{params[:token]}"
-      encoded_url = URI.encode(wow_get_next_page_url)
-      uri = URI.parse(encoded_url)
-      @places = JSON.parse(Net::HTTP.get(uri))      
-      @auto_complete_str = params[:auto_complete_str]
-      @auto_complete_lat = params[:auto_complete_lat]
-      @auto_complete_lng = params[:auto_complete_lng]  
-      @pid = params[:pid]
-      @str = params[:str]
-      @opt = params[:opt]
-      @redirect = params[:redirect]       
-      get_api_count   
-    else
-      wow_search_pid_url = ENV["api_search_by_pid_url"] + "?pid=#{params[:pid]}&opt=#{params[:opt]}"
-      encoded_url = URI.encode(wow_search_pid_url)
-      uri = URI.parse(encoded_url)
-      @places = JSON.parse(Net::HTTP.get(uri))      
-      @auto_complete_str = params[:str]
-      @auto_complete_lat = @places["auto_complete_lat"] 
-      @auto_complete_lng = @places["auto_complete_lng"]       
-      @pid = params[:pid]
-      @str = params[:ori_str]
-      @opt = params[:ori_opt]
-      @redirect = params[:redirect]    
-      get_api_count
-    end
+  def statistics
+    get_statistics
   end
 
   private
-  def get_api_count
-    wow_api_count_url = ENV["api_get_api_count_url"]
-    encoded_url = URI.encode(wow_api_count_url)
+  def get_statistics
+    wow_api_statistics_url = ENV["api_get_statistics_url"]
+    encoded_url = URI.encode(wow_api_statistics_url)
     uri = URI.parse(encoded_url)
-    @api_count = JSON.parse(Net::HTTP.get(uri))    
+    @statistics = JSON.parse(Net::HTTP.get(uri))    
+
+    @statistics["keyword_count"].sort_by! { |k| -k["count"] }
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get   'index', to: 'pages#index'  
-  post  'index', to: 'pages#index'  
   get   'detail', to: 'pages#detail'    
-  get   'near_by', to: 'pages#near_by'  
-
+  get   'nearby', to: 'pages#nearby'  
+  post  'keyword', to: 'pages#keyword'      
+  get   'keyword', to: 'pages#keyword'    
+  get   'statistics', to:'pages#statistics'
   root :to => 'pages#index'
 end
